c32f6a98e632ac210a9439baaa372fa0d08bff82
Oliver Szabo <oleewere@gmail.com>
2023-01-07 23:15:13 +0100

    Create alice wallet

--- it/devnet/containers/bitcoind-electrs/bin/start.sh
+++ it/devnet/containers/bitcoind-electrs/bin/start.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 nohup bitcoin/bin/bitcoind -regtest >/dev/stdout 2>&1 &
+sleep 5
+bitcoin/bin/bitcoin-cli -regtest createwallet "alice"
 nohup electrs -vvvv --daemon-dir /root/.bitcoin/ --network regtest --http-addr 0.0.0.0:3004 --cookie=devnet2:devnet2 --jsonrpc-import >/dev/stdout 2>&1 &
 tail -f /dev/stdout
